0ac117eb88177a78360596485f131d7e1757a680
unknown <once6474@naver.com>
2026-03-20 19:09:48 +0900

    backend_member_setting_03.20

--- backend/src/main/java/com/example/demo/dto/CreateTodoRequest.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.dto;
-
-import lombok.Getter;
-
-@Getter
-public class CreateTodoRequest {
-
-    private String content;
-}
--- backend/src/main/java/com/example/demo/dto/UpdateTodoRequest.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.dto;
-
-import lombok.Getter;
-
-@Getter
-public class UpdateTodoRequest {
-    private String content;
-    private boolean done;
-}

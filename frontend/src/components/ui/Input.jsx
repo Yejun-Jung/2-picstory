3552404c38fadfdbe96b862dd9402dc3c5cc7363
unknown <once6474@naver.com>
2026-03-24 19:52:05 +0900

    feat: front setting 1

--- frontend/src/components/ui/Input.jsx
+++ frontend/src/components/ui/Input.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from "react";
+import "./Input.scss";
 
-const Input = () => {
+const Input = ({ 
+  type="text", 
+  placeholder, 
+  name,
+  value, 
+  onChange, 
+  onBlur, 
+  error,
+  label,
+  icon,
+  disabled=false,
+  className='',
+  ...props 
+}) => {
   return (
-    <div>Input</div>
-  )
-}
+    <div className="input-group">
+      {label && <label className="input-label">{label}</label>}
+      <div className="input-wrapper">
+        {icon && <span className="input-icon">{icon}</span>}
+        <input 
+          type={type} 
+          name={name}
+          value={value}
+          placeholder={placeholder} 
+          onChange={onChange}
+          onBlur={onBlur}
+          disabled={disabled}
+          className="input-field"
+          {...props} 
+        />
+      </div>
+      {error && <p className="input-error">{error}</p>}
+    </div>
+  );
+};
 
-export default Input
+export default Input;
